9c26a3339a4fb43337e6412ca5bf42ea770760fd
Kirill Penchukov <kirill.penchukov.993@gmail.com>
2023-11-05 00:07:44 +0000

    Added deploy job to the CI/CD pipeline and changed a welcome API message to track changes in production

--- app/main.py
+++ app/main.py
@@ -29,4 +29,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to Kirill's API!"}
+    return {"message": "Welcome to Kirill's API - successfully deployed by GitHub Actions"}
